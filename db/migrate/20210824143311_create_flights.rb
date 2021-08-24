7c58c3f20231e4e580a6a8b64a447c9774ca9c2f
Grimhamr <rossiadisj@gmail.com>
2021-08-24 19:21:14 +0300

    migrate, fix typo, add references later

--- db/migrate/20210824143311_create_flights.rb
+++ db/migrate/20210824143311_create_flights.rb
@@ -1,15 +1,15 @@
 class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
-      t.date, :flight_date
-      t.time, :departure_time
-      t.time, :arrival_time
+      t.date :flight_date
+      t.time :departure_time
+      t.time :arrival_time
 
 
       t.timestamps
     end
 
-    add_reference :flights, :departure_airport, foreign_key: true
-    add_reference :flights, :destination_airport, foreign_key: true
+    #add_reference :flights, :departure_airport, foreign_key: true
+    #add_reference :flights, :destination_airport, foreign_key: true
   end
 end
