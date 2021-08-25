a2dd983032460c0164ff212697e49c2bdbb12cb8
Grimhamr <rossiadisj@gmail.com>
2021-08-25 20:45:32 +0300

    build select form, add options in controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
 
     def index
         @flights = Flight.all
-        @airports= Airport.all
+        @airport_options= Airport.all.map{ |e| [ e.name, e.id ]}
+        @available_dates = Flight.all.collect{ |flight| flight.flight_date}.sort
     end
 end
