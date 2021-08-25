18f0d8aefda9e504220028b0b044cbe084dacd95
Grimhamr <rossiadisj@gmail.com>
2021-08-25 10:37:04 +0300

    Add more comprehensive layout

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -22,4 +22,9 @@ class AirportsController < ApplicationController
     def index
         @airports = Airport.all
     end
+
+
+    def show
+        @airport = Airport.find(params[:id])
+    end
 end
