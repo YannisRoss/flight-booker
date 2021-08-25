8a37ee43ba7bf361cb72b9c7e30ee21ef2e0e924
Grimhamr <rossiadisj@gmail.com>
2021-08-25 08:39:45 +0300

    airports and flights index pages

---
+++ app/controllers/airports_controller.rb
@@ -0,0 +1,25 @@
+class AirportsController < ApplicationController
+
+
+    def new
+        @airport = Airport.new
+    end
+
+    def create
+        @airport = Airport.new
+
+        respond_to do |format|
+            if @airport.save
+              format.html { redirect_to @airport, notice: "airport was successfully created." }
+              format.json { render :show, status: :created, location: @airport }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @airport.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def index
+        @airports = Airport.all
+    end
+end
