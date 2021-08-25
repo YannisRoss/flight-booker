560136f00038416e6c715671ed677b3df20cfe4a
Grimhamr <rossiadisj@gmail.com>
2021-08-25 10:03:06 +0300

    change foreign keys to show proper paths

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,8 @@
 class Airport < ApplicationRecord
 
-    has_many :outbound_flights, foreign_key: "outbound_flight_id", class_name: "Flight", inverse_of: "departure_airport"
-    has_many :inbound_flights, foreign_key: "inbound_flight_id", class_name: "Flight", inverse_of: "destination_airport"
+    has_many :outbound_flights, class_name: "Flight", inverse_of: "departure_airport", foreign_key: "departure_airport_id"
+    has_many :inbound_flights, class_name: "Flight", inverse_of: "destination_airport", foreign_key: "destination_airport_id"
 
 end
+#  has_many :outbound_flights, class_name: "Flight", inverse_of: "departure_airport"
+#has_many :inbound_flights, class_name: "Flight", inverse_of: "destination_airport"
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,12 @@
 class Flight < ApplicationRecord
 
-    belongs_to :departure_airport, foreign_key: "departure_airport_id", class_name: "Airport"
-    belongs_to :destination_airport, foreign_key: "destination_airport_id", class_name: "Airport"
+    belongs_to :departure_airport,  class_name: "Airport", foreign_key: "departure_airport_id"
+    belongs_to :destination_airport, class_name: "Airport", foreign_key: "destination_airport_id"
 
 
 end
+
+
+#  belongs_to :departure_airport,  class_name: "Airport", foreign_key: "departure_airport_id"
+#belongs_to :destination_airport, class_name: "Airport", foreign_key: "destination_airport_id"
+
