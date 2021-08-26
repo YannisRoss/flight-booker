e81eae98841583d1426dddf304736f39783a485b
Grimhamr <rossiadisj@gmail.com>
2021-08-26 09:55:33 +0300

    fix passengers_number and flight.id params in form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,6 @@ class FlightsController < ApplicationController
     def index
         @flights = Flight.all
         @airport_options= Airport.all.map{ |e| [ e.name, e.id ]}
-        @available_dates = Flight.all.collect{ |flight| flight.flight_date}.sort
+        @available_dates = Flight.all.collect{ |flight| flight.flight_date}.sort.uniq
     end
 end
