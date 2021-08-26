0d4d206b126c11585bdb2c8aa1ccaf9c9935b100
Grimhamr <rossiadisj@gmail.com>
2021-08-26 18:02:28 +0300

    add loop for passenger details form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :airports
   resources :flights
   resources :bookings
+  resources :passengers
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
